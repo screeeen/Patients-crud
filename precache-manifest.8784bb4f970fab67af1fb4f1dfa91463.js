525ea89d909a50a1cd2689deda90bfbcc4217907
screeeen <miguel@screenimplosion.com>
2020-04-17 01:57:27 +0200

    Updates

---
+++ precache-manifest.8784bb4f970fab67af1fb4f1dfa91463.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2a7da9d9acc971060fd8ad5be1579b",
+    "url": "/Patients-crud/index.html"
+  },
+  {
+    "revision": "59b42303f7f61b4ee588",
+    "url": "/Patients-crud/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "03de07edb8673b16fc91",
+    "url": "/Patients-crud/static/css/main.ade940aa.chunk.css"
+  },
+  {
+    "revision": "59b42303f7f61b4ee588",
+    "url": "/Patients-crud/static/js/2.7782efe9.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/Patients-crud/static/js/2.7782efe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03de07edb8673b16fc91",
+    "url": "/Patients-crud/static/js/main.a9a26e46.chunk.js"
+  },
+  {
+    "revision": "8c40643f40d0334a85a8",
+    "url": "/Patients-crud/static/js/runtime-main.14ce1628.js"
+  }
+]);
